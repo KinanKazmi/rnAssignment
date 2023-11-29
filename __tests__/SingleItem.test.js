0f291ecba723932ee2f3433dee1d3fb66a13df76
KinanKazmi <kinan.kazmi@gmail.com>
2023-11-29 16:48:15 +0500

    Delete __tests__/SingleItem.test.js
    Removed unused files

--- __tests__/SingleItem.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SingleItem from '../src/Views/Home/SingleItem';
-import {render} from '@testing-library/react-native';
-
-it('should add to basket', () => {
-  const {getByText} = render(<SingleItem />);
-  // const addToBasket = getByText('Add to basket');
-  // fireEvent.press(addToBasket);
-});
